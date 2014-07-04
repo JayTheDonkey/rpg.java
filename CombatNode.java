113763e3bc961d92490cfefa0e13a3b92e3b2358
JayTheDonkey <JayAnder3.14159@gmail.com>
2014-07-04 12:42:40 -0700

    this is what i have so
    simple first draft of the combat node. need special equations

---
+++ CombatNode.java
@@ -0,0 +1,20 @@
+public class CombatNode extends Node {
+  public int getEnemies() {
+    return enemies;
+  }
+  public int killEnemies() {
+    enemies--;
+    return enemies;
+  }
+  public boolean combatOver() {
+    return (enemies <= 0);
+  }
+  public int addEnemies(int numSummoned){
+    return (enemies + numSummoned);
+  }
+  public int xp (int monstherRating){ //monsterRating should be the average monsterRating of the monsters in the combat
+    return ( monsterRating * enemies );
+  }
+    
+  private int enemies = -1 //add an equation to randomly generate enemies
+}
