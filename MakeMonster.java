b3b45a48408eeaf21db57e0efd5a8a3b4f0ad97f
Gabriel Damon <gabrieldamon42@gmail.com>
2014-07-14 21:15:39 -0700

    way too many weapons

--- MakeMonster.java
+++ MakeMonster.java
@@ -1,6 +1,6 @@
 public class MakeMonster {
   public static Monster skeleton() {
-    return new Monster("Skeleton", new MeleeWeapon[]{MakeMeleeWeapon.sword("rusty")}, new RangedWeapon[]{MakeRangedWeapon.rock("boring")}, 20, 6, 4, 0, 15);
+    return new Monster("Skeleton", new MeleeWeapon[]{MakeMeleeWeapon.shortSword("rusty")}, new RangedWeapon[]{MakeRangedWeapon.rock("boring")}, 20, 6, 4, 0, 15);
    }
  public static Monster skeletonArcher() {
    return new Monster("Skeleton Archer", new MeleeWeapon[]{MakeMeleeWeapon.brassKnuckles("old")}, new RangedWeapon[]{MakeRangedWeapon.bow("rotten")}, 10, 6, 4, 0, 17);
@@ -12,7 +12,7 @@ public class MakeMonster {
   return new Monster("Armoured Skeleton", new MeleeWeapon[]{MakeMeleeWeapon.axe("rusty")}, new RangedWeapon[]{MakeRangedWeapon.rock("iron")}, 30, 10, 4, 0, 25);
   }
  public static Monster flameArcher() {
-   return new Monster("Flame Archer", new MeleeWeapon[]{MakeMeleeWeapon.stick("flaming")}, new RangedWeapon[]{MakeRangedWeapon.enchantedBow("flaming")}, 12, 6, 4, 0, 20);
+   return new Monster("Flame Archer", new MeleeWeapon[]{MakeMeleeWeapon.baton("flaming")}, new RangedWeapon[]{MakeRangedWeapon.enchantedBow("flaming")}, 12, 6, 4, 0, 20);
   }
   public static Monster skeletalWizard() {
    return new Monster("Skeletal Wizard", new MeleeWeapon[]{MakeMeleeWeapon.staff("magic")}, new RangedWeapon[]{MakeRangedWeapon.fireball("large"), MakeRangedWeapon.ratSwarm("chittering")}, 8, 2, 2, 0, 30);
--- MakeRangedWeapon.java
+++ MakeRangedWeapon.java
@@ -1,20 +1,56 @@
 public class MakeRangedWeapon {
-	public static RangedWeapon rock(String descriptor) {
-		return new RangedWeapon("a " + descriptor + " rock", 1, -2);
-  	}
-  public static RangedWeapon bow(String descriptor) {
-  	return new RangedWeapon("a " + descriptor + " bow", 3, 6);
-  	}
-  public static RangedWeapon enchantedBow(String descriptor) {
-  	return new RangedWeapon("a " + descriptor + " enchanted bow", 6, 6);
-	  }
-	public static RangedWeapon crossbow(String descriptor) {
-  	return new RangedWeapon("a " + descriptor + " crossbow", 4, 2);
-  	}
+	public static RangedWeapon frostBurn(String descriptor) {
+		return new RangedWeapon(descriptor + " frost burn", 1, 3);
+	 	}
+	public static RangedWeapon magicMissile(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " magic missile", 2, 4);
+	 	}
 	public static RangedWeapon fireball(String descriptor) {
-		return new RangedWeapon("a " + descriptor + " fireball", 5, 2);
+		return new RangedWeapon("a " + descriptor + " fireball", 3, 2);
 	 	}
 	public static RangedWeapon ratSwarm(String descriptor) {
-		return new RangedWeapon("a " + descriptor + " swarm of rats", 8, -5);
+		return new RangedWeapon("a " + descriptor + " swarm of rats", 4, 5);
+		}
+	public static RangedWeapon arcaneCannon(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " arcane cannon", 5, 3);
+	 	}
+	public static RangedWeapon meteorStorm(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " meteor storm", 6, 4);
+	 	}
+	public static RangedWeapon thunderLance(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " thunder lance", 7, 0);
+	 	}
+	public static RangedWeapon lightCrossbow(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " light crossbow", 3, -1);
+		}
+	public static RangedWeapon crossbow(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " crossbow", 4, 0);
+		}
+	public static RangedWeapon heavyCrossbow(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " heavy crossbow", 5, 0);
+		}
+	public static RangedWeapon lightBow(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " light bow", 4, 3);
+		}
+	public static RangedWeapon bow(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " bow", 5, 3);
+		}
+	public static RangedWeapon heavyBow(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " heavy bow", 6, 3);
+		}
+	public static RangedWeapon enchantedBow(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " enchanted bow", 6, 5);
+		}
+	public static RangedWeapon sling(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " sling", 2, 0);
+		}
+	public static RangedWeapon boomerang(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " boomerang", 2, 3);
+		}
+	public static RangedWeapon trhowingAxe(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " trhowing axe", 3, 2);
+		}
+	public static RangedWeapon rock(String descriptor) {
+		return new RangedWeapon("a " + descriptor + " rock", 1, 0);
 		}
 	}
