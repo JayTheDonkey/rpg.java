6a944f14c3d5c9d65afde3ac734e2314f620dd46
JayTheDonkey <JayAnder3.14159@gmail.com>
2014-07-24 20:34:05 -0700

    i lied, now the spelling is fixed

--- Armor.java
+++ Armor.java
@@ -1,4 +1,4 @@
-public class Armor extends Defences{
+public class Armor extends Defenses{
  public Armor(String tempName, String tempDescriptor, int tempMinDex, int tempMinStr, int tempDefenseValue, int tempPrice, int tempDexDebuff, int tempSpeedDebuff, int tempMagicResistance) {
   super( tempName, tempDescriptor, tempMinDex, tempMinStr,  tempDefenseValue, tempPrice, tempDexDebuff,  tempSpeedDebuff, tempMagicResistance);
 }
--- Shield.java
+++ Shield.java
@@ -1,4 +1,4 @@
-public class Shield extends Defences {
+public class Shield extends Defenses {
  public Shield (String tempName, String tempDescriptor, int tempMinDex, int tempMinStr, int tempDefenseValue, int tempPrice, int tempDexDebuff, int tempSpeedDebuff, int tempMagicResistance) {
   super( tempName,tempDescriptor, tempMinDex, tempMinStr, tempDefenseValue, tempPrice, tempDexDebuff,  tempSpeedDebuff,  tempMagicResistance);
 }
