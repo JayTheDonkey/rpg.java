3449ecebaa0718085b43d635393b5e5e152e7e58
JayTheDonkey <JayAnder3.14159@gmail.com>
2014-07-24 20:33:05 -0700

    changed spelling error

--- Defenses.java
+++ Defenses.java
@@ -1,5 +1,5 @@
-public class Defences {
-	public Defences(String tempName, String tempDescriptor, int tempMinDex, int tempMinStr, int tempDefenseValue, int tempPrice, int tempDexDebuff, int tempSpeedDebuff, int tempMagicResistance) {
+public class Defenses {
+ public Defenses(String tempName, String tempDescriptor, int tempMinDex, int tempMinStr, int tempDefenseValue, int tempPrice, int tempDexDebuff, int tempSpeedDebuff, int tempMagicResistance) {
   name = tempName;
   descriptor = tempDescriptor;
   minDex = tempMinDex;
